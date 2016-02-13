c3f40422e969cead1ebb6ea38780cf1b24736e5b
Nickykiet83 <phamquanganhkiet@gmail.com>
2016-02-14 01:36:31 +0700

    Add varible playground area

---
+++ variable-playground.js
@@ -0,0 +1,31 @@
+//var person = {
+//    name: 'Nicky',
+//    age: 23
+//};
+//
+//function updatePerson(obj){
+//    //not right
+////    obj ={
+////        name: "Nicky",
+////        age: 33
+////    }
+//    
+//    //right
+//    obj.age = 33;
+//}
+//
+//updatePerson(person);
+//console.log(person);
+
+// Array Example
+var grades = [15, 88];
+
+function addGrades (gradesArr, newGrade){
+    grades.push(55);
+    debugger;
+    //gradesArr = [12, 33, 99];
+}
+
+
+addGrades(grades)
+console.log(grades);
