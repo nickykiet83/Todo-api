a0903985852ee92b2105cd9ffd0f29b8868493d1
Nickykiet83 <phamquanganhkiet@gmail.com>
2016-02-14 01:53:31 +0700

    Add ?q filtered variable

--- server.js
+++ server.js
@@ -15,21 +15,23 @@ app.get('/', function (req, res){
     res.send('Todo API Root'); 
 });
 
-// GET /todos?completed=true
+// GET /todos?completed=false&q=work
 app.get('/todos', function (req, res){
     //nhan ve cac tham so tren thanh dia chi
     var queryParams = req.query;
     var filteredTodos = todos;
     
-    // if has property && completed === 'true'
-    // filteredTodos = _.where(filteredTodos, ?)
-    // else if has prop && completed if 'false'
     if (queryParams.completed === 'true' && queryParams.hasOwnProperty('completed')){
         filteredTodos = _.where(filteredTodos, {completed: true});
     }else if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'false'){
         filteredTodos = _.where(filteredTodos, {completed: false});
     }
     
+    if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+		filteredTodos = _.filter(filteredTodos, function (todo) {
+			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+		});
+	}
     
     res.json(filteredTodos);
 });
